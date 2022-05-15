9f871d8a890bcca050f3549c33383044b8973cdf
Paul David Hiller <paul.david.hiller@mni.thm.de>
2022-05-15 14:09:39 +0200

    role and logged in guard added + facebook login added + css changes for login and login child comp + user data now stored in local storage

--- Juntos/src/app/models/classes/user.spec.ts
+++ Juntos/src/app/models/classes/user.spec.ts
@@ -2,6 +2,6 @@ import {User} from 'src/app/models/classes/user';
 
 describe('User', () => {
   it('should create an instance', () => {
-    expect(new User()).toBeTruthy();
+    expect(new User("testID", "test@test.de")).toBeTruthy();
   });
 });
--- Juntos/src/app/services/auth.service.ts
+++ Juntos/src/app/services/auth.service.ts
@@ -4,6 +4,8 @@ import firebase from "firebase/compat/app";
 import {UserDataService} from "src/app/services/user-data.service";
 import User from "src/app/models/classes/user";
 import GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
+import FacebookAuthProvider = firebase.auth.FacebookAuthProvider;
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,31 +14,42 @@ import GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 export class AuthService {
 
   user: User;
+  token;
   public email!: string;
   public password!: string;
 
   constructor(private afAuth: AngularFireAuth, private userDataService: UserDataService) {
     this.afAuth.authState.subscribe(async firebaseUser => {
-        if(firebaseUser){
+      console.log(firebaseUser);
+      this.user = undefined;
+      this.token = undefined;
+      if(firebaseUser){
           this.user = await this.userDataService.getUserById(firebaseUser.uid);
+          this.token = firebaseUser.getIdTokenResult(false);
         }
-        this.user = undefined;
+      localStorage.setItem('user', JSON.stringify(this.user));
+      localStorage.setItem('token', JSON.stringify(firebaseUser.getIdTokenResult(true)));
       }
-
     )
-    console.log(this.user);
+  }
 
+  isloggedin() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return user !== undefined;
+  }
+
+  hasRole(role){
+    //returns true if user role equivalent or smaller
+    return Number(role) >= Number(JSON.parse(localStorage.getItem('user')).rights);
   }
 
   checkEmailAndPasswort() {
     return this.email != undefined && this.password != undefined
       && this.email.trim().length > 3 && this.email.includes('@')
       && this.password.trim().length > 5;
-
   }
   //Login with E-Mail and password
   EmailLogin() {
-    console.log(this.user);
     if (this.checkEmailAndPasswort) {
       this.afAuth.signInWithEmailAndPassword(this.email, this.password)
         .then((userCredential) => {
@@ -69,9 +82,18 @@ export class AuthService {
     return this.AuthLogin(new GoogleAuthProvider(), userType);
   }
 
+  FacebookAuth(userType) {
+    //TODO: Nutzungsbedingungen URL & URL zur Datenrichtlinie bei Meta Developer hinzufügen
+    //https://www.devopsschool.com/blog/error-app-not-set-up-this-app-is-still-in-development-mode-and-you-dont-have-access-to-it/#:~:text=returns%20this%20error-,App%20not%20set%20up%3A%20This%20app%20is%20still%20in%20development,t%20login%20with%20their%20facebook.
+    return this.AuthLogin(new FacebookAuthProvider(), userType);
+  }
+
 
   // Auth logic to run auth providers
   AuthLogin(provider, userType) {
+    provider.setCustomParameters({
+      'display': 'popup'
+    });
     return this.afAuth
       .signInWithPopup(provider)
       .then((userCredential) => {
@@ -84,6 +106,13 @@ export class AuthService {
       .catch((error) => {
         console.log(error);
       });
+  }
+
+  SignOut() {
+    this.afAuth.signOut().then(() => {
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
+    })
   }
 
 
