00bce8ca9474daf13675389818e67380f3d0bdf6
Paul David Hiller <paul.david.hiller@mni.thm.de>
2022-05-19 17:40:48 +0200

    remove cordova firebase plugin add android add google-services,json
    changed app id to com.juntos.events

---
+++ Juntos/android/app/src/main/java/com/juntos/events/MainActivity.java
@@ -0,0 +1,5 @@
+package com.juntos.events;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- Juntos/capacitor.config.ts
+++ Juntos/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.juntos.events',
   appName: 'Juntos',
   webDir: 'www',
   bundledWebRuntime: false
