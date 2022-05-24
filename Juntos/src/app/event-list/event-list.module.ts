a0b32f65b1d75e96403a94a36b581c274180228b
Paul David Hiller <paul.david.hiller@mni.thm.de>
2022-05-24 17:03:25 +0200

    add img src to event list

--- Juntos/src/app/event-list/event-list.module.ts
+++ Juntos/src/app/event-list/event-list.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { EventListPageRoutingModule } from './event-list-routing.module';
-
 import { EventListPage } from './event-list.page';
-import {SharedModule} from "src/app/shared/shared.module";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
   imports: [
@@ -19,4 +16,5 @@ import {SharedModule} from "src/app/shared/shared.module";
   ],
   declarations: [EventListPage]
 })
+
 export class EventListPageModule {}
