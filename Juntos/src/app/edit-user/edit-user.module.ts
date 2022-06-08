bb9d0795f877af5de0fa7c2c12812a63897315ba
Daniel <gasevicdaniel@gmail.com>
2022-06-08 13:47:02 +0200

    edit user done, todo: documentation and tests

--- Juntos/src/app/edit-user/edit-user.module.ts
+++ Juntos/src/app/edit-user/edit-user.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { EditUserPageRoutingModule } from './edit-user-routing.module';
 import { EditUserPage } from './edit-user.page';
-import {SharedModule} from "../shared/shared.module";
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
